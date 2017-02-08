be710e85f693e4e5383fe9878bc8c5fe1cb40ccd
KEN <kilol@qq.com>
2017-02-08 11:45:54 +0800

    添加一个小测试

--- app/view/index/add.spec.js
+++ app/view/index/add.spec.js
@@ -1,6 +1,6 @@
 import {add} from './add';
 describe('add', () => {
-  it('是否符合add算法', (done) => {
-    expect(add(1, 2)).to.be.equal(3); // 正确参数 'INCREMENT'
-  })
+  it('1+2=3', function () {
+    expect(add(1,2)).to.be.equal(3);
+  });
 });
