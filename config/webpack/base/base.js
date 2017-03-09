b5e22deaf903acfbf41881db9278e91f234e1183
sayll <sayll>
2017-03-09 18:14:34 +0800

    修改端口为3000 输出文件优化为相对路劲

--- config/webpack/base/base.js
+++ config/webpack/base/base.js
@@ -1,9 +1,9 @@
 module.exports = {
   mainJS  : false, // 添加公共main.js
   devHost : '0.0.0.0',
-  devPort : '1000',
+  devPort : '3000',
   viewType: 'html', // pug,jade,html...
   cssType : 'css', // sass,scss,less,pcss,css...
-  cdnPath : '/', // 资源指向位置,可寄放CDN
+  cdnPath : './', // 资源指向位置,可寄放CDN
   version : '1.0.0' // 输出资源带版本号
 };
