4c477cbcf6541437b0e649e02d38863adf82729c
sayll <sayll>
2017-03-09 18:59:22 +0800

    添加线程处理

--- config/webpack/modules/plugins.js
+++ config/webpack/modules/plugins.js
@@ -2,19 +2,36 @@ const base = require('../base/base'),
       files     = require('../base/files'),
       HappyPack = require('happypack'),
       webpack   = require('webpack');
-let vendorsJson = files.dllPath + '/vendors.json';
+function cHappypack(id, loaders) {
+  return new HappyPack({
+    id: id,
+    debug: false,
+    verbose: false,
+    cache: true,
+    threads: 4,
+    cacheContext: {
+      env: process.env.NODE_ENV
+    },
+    loaders: loaders
+  })
+}
+
+const vendorsJson = files.dllPath + '/vendors.json';
 
 module.exports = [
   new webpack.NoEmitOnErrorsPlugin(),
+
   new webpack.DefinePlugin({
     'process.env': {
       NODE_ENV: JSON.stringify(process.env.NODE_ENV),
     },
   }),
+
   new webpack.DllReferencePlugin({
     context : '/',
     manifest: require(vendorsJson)
   }),
+
   new webpack.LoaderOptionsPlugin({
     minimize: true,
     options : {
@@ -51,15 +68,11 @@ module.exports = [
         ]:[require('autoprefixer')({browsers: ['> 1%', 'last 5 versions', 'Firefox ESR'], cascade: false})]
     }
   }),
-  new HappyPack({
-    id          : 'cJSX',
-    cache       : true,
-    cacheContext: {
-      env: process.env.NODE_ENV
-    },
-    loaders     : [{
-      loader: 'babel-loader',
-      query : require('./babel')
-    }]
-  })
+
+  cHappypack('HTML', ['html-loader']),
+
+  cHappypack('JSX', [{
+    loader: 'babel-loader',
+    query: require('./babel')
+  }]),
 ];
